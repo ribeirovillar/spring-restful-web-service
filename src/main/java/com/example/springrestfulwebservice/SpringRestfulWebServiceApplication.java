9ade9ce90b9a9c270484dcdee3517e984ec3a8e6
villar <ribeiros@outlook.com>
2022-11-06 10:27:31 +0000

    spring project started

---
+++ src/main/java/com/example/springrestfulwebservice/SpringRestfulWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrestfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestfulWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestfulWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springrestfulwebservice/SpringRestfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrestfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestfulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
